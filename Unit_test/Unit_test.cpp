8055dd36abe4339e75446bee85c96ce819793e8d
Marek Morawiec <marek@kartograf.com.pl>
2016-12-14 13:05:05 +0100

    commit 4

--- Unit_test/Unit_test.cpp
+++ Unit_test/Unit_test.cpp
@@ -5,7 +5,7 @@
 #include <boost/test/unit_test.hpp>
 #include "../boost_unit_tests/testowany program.h"
 
-// test stash
+// test stash 11
 
 BOOST_AUTO_TEST_SUITE(Matematyka_TESTY)
 BOOST_AUTO_TEST_CASE(TestDodawania)
